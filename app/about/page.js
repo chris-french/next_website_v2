e2058281cba8fd54085972659597e244079a9ea1
chris-french <chris.french.dev@gmail.com>
2024-09-10 19:07:42 -0500

    fixing more typos

--- app/about/page.js
+++ app/about/page.js
@@ -9,10 +9,10 @@ export default function About() {
                 Professional programmer with an emphasis on full-stack development and data engineering using Python, C++, C#, SQL, NodeJS, and React. I have multiple years of experience working in the cloud, especially Azure and AWS.
             </BodyBox>
             <BodyBox>
-                I also spend time making video games, using Godot and Unreal Engine 5.<br/><br/>I'm currently working on a C++ module for Godot using the EnTT entity component system for 4x Strategy games. Most of my time is spend working with Godot in C++ and GDScript, especially UI/UX design and game mechanics.
+                I also spend time making video games, using Godot and Unreal Engine 5.<br/><br/>I'm currently working on a C++ module for Godot using the EnTT entity component system for 4x Strategy games.<br /><br />Most of my free time is spent working with Godot in C++ and GDScript, especially UI/UX design and game mechanics.
             </BodyBox>
             <BodyBox>
-                I did my PhD working on the philosophy of science, and I have a minor in Computer Science.<br /><br/>However, I've been programming since I was a teenager, and I've done everything from technical writing, to writing tools for teams, to data engineering, to UI/UX design, to working on backend systems, and to working on a developer experience team. I'm now employeed as a Business Intelligence Developer. Oh, and I also write science fiction and enjoy oil painting.
+                I did my PhD working on the philosophy of science (history of analytic philosophy, logic, foundations of probability theory, foundations of biology), and I have a minor in Computer Science.<br /><br/>However, I've been programming since I was a teenager, and I've done everything from technical writing, to writing tools for doing analytics, to data engineering, to UI/UX design, to working on backend systems, and to working on a developer experience team. I'm now employeed as a Business Intelligence Developer. Oh, and I also write science fiction and enjoy oil painting.
             </BodyBox>
 
             <BodyBox>
